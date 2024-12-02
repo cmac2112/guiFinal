ab8874279af988e35c4c8b480adeec3445397138
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-12-02 10:49:56 -0600

    working on dashboard

--- guiFinalClient/src/app/dashboard/dashboard.component.ts
+++ guiFinalClient/src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Event{
+  id: number;
+  event_name: string;
+  description: string;
+  created_by: string;
+  end_time: string;
+  start_time: string;
+  location: string;
+  created_at: string;
+}
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+
+export class DashboardComponent implements OnInit {
   //get events
   //allow user to create events
   //allow user to join events
   //allow user to leave events
+  events: Event[] = [];
+  ngOnInit() {
+    //get events
+    this.getEvents();
+  }
+  async getEvents() {
+    //get events
+    const response = await fetch(`https://gui230.jitdesigns.com/api/events`);
+    const data: Event[] = await response.json();
+    this.events = data
+    console.log(this.events)
+  }
+
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const options: Intl.DateTimeFormatOptions = {
+      month: '2-digit',
+      day: '2-digit',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+  };
+  return date.toLocaleString('en-US', options);
+}
 }
--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f56f01f1b988f7ecf466d00ebe5687dfa202272")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79f77979aea0f54b9754cc933fa07b6ca37e9f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
